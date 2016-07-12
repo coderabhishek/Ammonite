0358959461decbb92439eb1c69386df894e13553
coderabhishek <abhishekkmr18@gmail.com>
2016-07-12 16:16:13 +0530

    try l

--- integration/src/test/scala/ammonite/integration/LineNumberTests.scala
+++ integration/src/test/scala/ammonite/integration/LineNumberTests.scala
@@ -17,6 +17,7 @@ object LineNumberTests extends TestSuite{
         exec(file)
       }.result
       val e = res.err.string
+      println(res.out.string)
       println("4444444444\n" + e + "\n" + expected + "\n3333333333333333333333")
       println("$$$$$$$$$$\n" + e.map(_.toInt) + "\n" + expected.map(_.toInt) + "\n######################")
       assert(e.contains(expected.replace("\n", System.lineSeparator())))
